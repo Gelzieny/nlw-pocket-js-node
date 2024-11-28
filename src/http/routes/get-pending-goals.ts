d5ea824d2a84e2d571a27f64c256067ec3fb1317
Gelzieny R. Martins <gelzieny@gmail.com>
2024-11-27 21:05:53 -0300

    Documentando Rotas da API

--- src/http/routes/get-pending-goals.ts
+++ src/http/routes/get-pending-goals.ts
@@ -1,8 +1,29 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { getWeekPendingGoals } from '../../functions/get-week-pending-goals'
+import z from 'zod'
 
 export const getPendingGoalsRoute: FastifyPluginAsyncZod = async app => {
-  app.get('/pending-goals', async () => {
+  app.get('/pending-goals', 
+    {
+      schema: {
+        tags: ['goals'],
+        operationId: 'getPendingGoals',
+        description: 'Get pending goals',
+        response: {
+          200: z.object({
+            pendingGoals: z.array(
+              z.object({
+                id: z.string(),
+                title: z.string(),
+                desiredWeeklyFrequency: z.number(),
+                completionCount: z.number(),
+              })
+            ),
+          }),
+        },
+      },
+    },
+    async ()  => {
     const { pendingGoals } = await getWeekPendingGoals()
 
     return { pendingGoals }
--- src/http/routes/get-week-summary.ts
+++ src/http/routes/get-week-summary.ts
@@ -1,8 +1,42 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { getWeekSummary } from '../../functions/get-week-summary'
+import z from 'zod'
+import dayjs from 'dayjs'
 
 export const getWeekSummaryRoute: FastifyPluginAsyncZod = async app => {
-  app.get('/summary', async () => {
+  app.get('/summary', 
+    {
+      schema: {
+        tags: ['goals'],
+        operationId: 'getWeekSummary',
+        description: 'Get completed goals in a specific week',
+        querystring: z.object({
+          weekStartsAt: z.coerce
+            .date()
+            .optional()
+            .default(dayjs().startOf('week').toDate()),
+        }),
+        response: {
+          200: z.object({
+            summary: z.object({
+              completed: z.number(),
+              total: z.number(),
+              goalsPerDay: z.record(
+                z.string(),
+                z.array(
+                  z.object({
+                    id: z.string(),
+                    title: z.string(),
+                    completedAt: z.string(),
+                  })
+                )
+              ),
+            }),
+          }),
+        },
+      },
+    },
+    async ()  => {
     const { summary } = await getWeekSummary()
 
     return { summary }
